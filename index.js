d27d74992cfc616eabcf7fa3a0c5351da80b18ae
Mahmudjon Shukurov <sukurovmahmudzon15@gmail.com>
2024-04-17 09:06:54 +0300

    tugadi

---
+++ index.js
@@ -0,0 +1,22 @@
+let taxminiySon1 = Math. random();
+let taxminiySon2 = Math. random();
+let taxminiySon3 = Math. random();
+
+console.log(taxminiySon1);
+console.log(taxminiySon1 *2);
+console.log(taxminiySon1 /2)
+console.log(taxminiySon1 %2)
+
+console.log(taxminiySon2);
+console.log(taxminiySon2 *2);
+console.log(taxminiySon2 /2)
+console.log(taxminiySon2 %2)
+
+
+console.log(taxminiySon3);
+console.log(taxminiySon3 *2);
+console.log(taxminiySon3 /2)
+console.log(taxminiySon3 %2)
+
+
+
